c8a7d669b9ab83cca58408d9b48f4807b001c97b
Dennis Lee <dennis.lee@xendit.co>
2024-05-10 15:38:19 +0800

    feat: form validation

--- src/app/CreateForm.tsx
+++ src/app/CreateForm.tsx
@@ -2,17 +2,32 @@
 
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
 
 import { Stack, Button, TextField } from "@mui/material";
+import { isURL } from "validator";
+
+type FormValues = {
+  url: string;
+};
+
+const URL_VALIDATOR_OPTIONS = {
+  require_protocol: false,
+};
 
 export default function CreateForm() {
+  const form = useForm<FormValues>({
+    defaultValues: {
+      url: "",
+    },
+  });
   const router = useRouter();
 
-  const [url, setUrl] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const { control, register, handleSubmit, formState, reset } = form;
+  const { errors } = formState;
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const onSubmit = async (data: FormValues) => {
     setIsLoading(true);
 
     await fetch(`${process.env.NEXT_PUBLIC_URL_SHORTENER_API}/url`, {
@@ -21,29 +36,33 @@ export default function CreateForm() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        url,
+        url: data.url,
       }),
     }).then((res) => {
       if (res.status === 200) {
         router.refresh();
-        setUrl("");
       }
 
+      reset();
       setIsLoading(false);
     });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)} noValidate>
       <Stack spacing={2}>
         <TextField
           variant="outlined"
           margin="normal"
           fullWidth
-          required
-          onChange={(e) => setUrl(e.target.value)}
-          value={url}
           placeholder="Enter a URL to shorten"
+          {...register("url", {
+            required: "URL is required",
+            validate: (value, formValues) =>
+              isURL(value, URL_VALIDATOR_OPTIONS) || "Invalid URL",
+          })}
+          error={!!errors.url}
+          helperText={errors.url?.message}
         />
         <Button
           type="submit"
